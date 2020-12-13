63d01718f6771fe36ad377d9ac34ad319e643f73
Chris Rabe <chris.rabe14@gmail.com>
2020-12-13 19:32:05 +1000

    updated bomb placement to get nearest empty tile

--- wizard_agent/brain/bomb_placement_strategy.py
+++ wizard_agent/brain/bomb_placement_strategy.py
@@ -6,68 +6,39 @@ utils = _utils.util_functions
 constants = _utils.constants
 
 
-def _get_nearest_wood_block(location, wood_block_list):
-    if wood_block_list:
-        wood_block_distance = 10
-        closest_wood_block = wood_block_list[0]
-        for wood_block in wood_block_list:
-            new_wood_block_dist = utils.manhattan_distance(location, wood_block)
-            if new_wood_block_dist < wood_block_distance:
-                wood_block_distance = new_wood_block_dist
-                closest_wood_block = wood_block
-        return closest_wood_block
-    else:
-        return None
-
-
-def get_tile_next_to_block(location, tiles, wood_blocks):
-    """
-    Given a list of tiles and wooden blocks, find the tile that's closest empty tile to move to
-    """
-
-    block_distance = 10
-    closest_block = wood_blocks[0]
-
-    for block in wood_blocks:
-        new_block_distance = utils.manhattan_distance(block, location)
-        if new_block_distance < block_distance:
-            block_distance = new_block_distance
-            closest_block = block
-
-    empty_tile_near_block_dict = {}
-    for tile in tiles:
-        distance = utils.manhattan_distance(closest_block, tile)
-        empty_tile_near_block_dict[tile] = distance
-
-    # return the tile with the furthest distance from any bomb
-
-    if len(empty_tile_near_block_dict) > 0: 
-        best_tile = min(empty_tile_near_block_dict, key=empty_tile_near_block_dict.get)
-        return best_tile
-    else:
-        return None
-    
-
 class BombPlacementStrategy(strategy.Strategy):
     def execute(self, game_state: object, player_state: object) -> List[str]:
         wood_blocks = game_state.soft_blocks
         location = player_state.location
-        # get the nearest block to the player
-        nearest_wood_block_location = _get_nearest_wood_block(location, wood_blocks)
 
-        surrounding_tiles = utils.get_surrounding_tiles(nearest_wood_block_location, game_state)
+        # get the nearest block to the player
+        reachable_wood_blocks = utils.get_reachable_tiles(location, wood_blocks, game_state)
+        nearest_wood_block = utils.get_nearest_tile(location, reachable_wood_blocks)
+
+        surrounding_tiles = utils.get_surrounding_tiles(nearest_wood_block, game_state)
         empty_tiles = utils.get_empty_tiles(surrounding_tiles, game_state)
 
         # navigate to the wood_block
-        if nearest_wood_block_location is not None:
-            tile_near_block = get_tile_next_to_block(location, empty_tiles, wood_blocks)
-            path = utils.get_shortest_path(location, tile_near_block, game_state)
-            action_seq = utils.get_path_action_seq(location, path)
-            action_seq.append(constants.ACTIONS["bomb"])
-            return action_seq
-        else:
-            return [constants.ACTIONS["none"]]
+        if nearest_wood_block is not None:
+            tile_near_block = utils.get_nearest_tile(location, empty_tiles)
+            if tile_near_block:
+                path = utils.get_shortest_path(location, tile_near_block, game_state)
+                action_seq = utils.get_path_action_seq(location, path)
+                action_seq.append(constants.ACTIONS["bomb"])
+                return action_seq
+        return [constants.ACTIONS["none"]]
 
     def can_execute(self, game_state: object, player_state: object) -> bool:
         ammo = player_state.ammo
-        return ammo > 0
+
+        wood_blocks = game_state.soft_blocks
+        location = player_state.location
+
+        # get the nearest block to the player
+        reachable_wood_blocks = utils.get_reachable_tiles(location, wood_blocks, game_state)
+        nearest_wood_block = utils.get_nearest_tile(location, reachable_wood_blocks)
+
+        surrounding_tiles = utils.get_surrounding_tiles(nearest_wood_block, game_state)
+        empty_tiles = utils.get_empty_tiles(surrounding_tiles, game_state)
+
+        return ammo > 0 and len(empty_tiles) > 0
