d62444f4cd68240f8612b1bcb1f6294f7a8e1822
Yvonne Tansu <yvonne.tansu@gmail.com>
2020-12-13 08:36:01 +0700

    added treasure strategy

--- wizard_agent/my_agent.py
+++ wizard_agent/my_agent.py
@@ -25,6 +25,26 @@ def get_bombs_in_range(location, bombs):
             bombs_in_range.append(bomb)
     return bombs_in_range
 
+def get_treasure_in_range(location, treasure, game_state):
+    treasures = get_reachable_treasure(location, treasure, game_state)
+    if treasure:
+        treasure_in_range = []
+        for treasure in treasures:
+            distance = utils.manhattan_distance(location, treasure)
+            if distance <= 10:
+                treasure_in_range.append(treasure)
+        return treasure_in_range
+    else:
+        return []
+
+def get_reachable_treasure(location, treasure_list, game_state: object):
+    list_treasures = []
+    for treasure in treasure_list:
+        path = utils.get_shortest_path(location, treasure, game_state)
+        if path:
+            list_treasures.append(treasure)
+    return list_treasures
+
 
 class Agent:
     def __init__(self):
@@ -33,7 +53,8 @@ class Agent:
             'flee': brain.FleeStrategy(),
             'move': brain.MoveStrategy(),
             'bomb': brain.BasicBombStrategy(),
-            'reload': brain.ReloadStrategy()
+            'reload': brain.ReloadStrategy(),
+            'treasure': brain.TreasureStrategy()
         }
         self.action_queue = []
 
@@ -45,19 +66,24 @@ class Agent:
             strategy_name = "random"
             location = player_state.location
             bombs = game_state.bombs
+            treasures = game_state.treasure
             ammo = player_state.ammo
 
             bombs_in_range = get_bombs_in_range(location, bombs)
+            treasure_in_range = get_treasure_in_range(location, treasures, game_state)
 
             # Determine next action
             if game_state.entity_at(location) == 'b':
                 strategy_name = 'move'
+            elif treasure_in_range:
+                strategy_name = 'treasure'
             elif bombs_in_range:
                 strategy_name = 'flee'
             elif ammo > 0:
                 strategy_name = 'bomb'
             elif ammo == 0:
                 strategy_name = 'reload'
+            
 
             # enqueue next action sequence
             strategy = self.strategies[strategy_name]
