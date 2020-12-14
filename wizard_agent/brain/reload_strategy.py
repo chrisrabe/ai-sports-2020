d7d4822fb6b819b5c60ab7e2aff2a4e1bfdb68fd
Chris Rabe <chris.rabe14@gmail.com>
2020-12-14 20:37:09 +1000

    updated execution conditions of reload strategy

--- wizard_agent/brain/reload_strategy.py
+++ wizard_agent/brain/reload_strategy.py
@@ -39,7 +39,6 @@ class ReloadStrategy(strategy.Strategy):
     def execute(self, game_state: object, player_state: object) -> List[str]:
         ammo = game_state.ammo
         location = player_state.location
-        bombs = game_state.bombs
         list_of_opponents = game_state.opponents(player_state.id)
         opponent_location = utils.get_opponent(location, list_of_opponents)
 
@@ -52,29 +51,37 @@ class ReloadStrategy(strategy.Strategy):
 
         # navigate to the ammo
         if nearest_ammo is not None:
-            if not utils.is_opponent_closer(location, opponent_location, nearest_ammo) and utils.is_safe_path(location, nearest_ammo, bombs, game_state):
+            if not utils.is_opponent_closer(location, opponent_location, nearest_ammo):
                 path = utils.get_shortest_path(location, nearest_ammo, game_state)
                 action_seq = utils.get_path_action_seq(location, path)
                 return action_seq
 
         if furthest_ammo_from_opponent is not None:
-            if utils.is_safe_path(location, furthest_ammo_from_opponent, bombs, game_state):
-                path = utils.get_shortest_path(location, furthest_ammo_from_opponent, game_state)
-                action_seq = utils.get_path_action_seq(location, path)
-                return action_seq
+            path = utils.get_shortest_path(location, furthest_ammo_from_opponent, game_state)
+            action_seq = utils.get_path_action_seq(location, path)
+            return action_seq
 
         return [constants.ACTIONS["none"]]
 
     def can_execute(self, game_state: object, player_state: object) -> bool:
         player_ammo = player_state.ammo
+        bombs = game_state.bombs
         location = player_state.location
         list_of_opponents = game_state.opponents(player_state.id)
         opponent_location = utils.get_opponent(location, list_of_opponents)
         ammo_pickups = game_state.ammo
         ammo_in_range = utils.get_reachable_tiles(location, ammo_pickups, game_state)
+
+        # define targets
         furthest_ammo = _get_furthest_ammo_from_opponent(opponent_location, ammo_in_range)
         nearest_ammo = _get_nearest_ammo(location, ammo_in_range)
+
+        # define execution conditions
         is_opponent_closer = utils.is_opponent_closer(location, opponent_location, nearest_ammo)
-        can_reach_nearest_ammo = nearest_ammo is not None and not is_opponent_closer
-        can_reach_any_ammo = furthest_ammo is not None or can_reach_nearest_ammo
+        closest_ammo_safe = utils.is_safe_path(location, nearest_ammo, bombs, game_state)
+        furthest_ammo_safe = utils.is_safe_path(location, furthest_ammo, bombs, game_state)
+        can_reach_nearest_ammo = nearest_ammo is not None and not is_opponent_closer and closest_ammo_safe
+        can_reach_furthest_ammo = furthest_ammo is not None and furthest_ammo_safe
+        can_reach_any_ammo = can_reach_furthest_ammo or can_reach_nearest_ammo
+
         return player_ammo < 3 and can_reach_any_ammo
